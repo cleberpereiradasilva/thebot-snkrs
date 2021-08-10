572c821251cc9d3410650ef6ce02aaf3694bc24f
Cleber Pereira da Silva <cleberpereira@cvccorp.com.br>
2021-08-10 11:24:32 -0300

    feat: wip in message format

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 
 
-from crawler.crawler.spiders.nike_snkrs_spider import NikeSnkrsSpider
+from crawler.crawler.spiders.nike_restock_spider import NikeSnkrsSpider
 from crawler.crawler.spiders.nike_calendario_spider import NikeCalendarioSpider
-from crawler.crawler.spiders.nike_novidades_spider import NikeNovidadesSpider
+from crawler.crawler.spiders.nike_lancamentos_spider import NikeNovidadesSpider
 
 from crawler.crawler.spiders.maze_snkrs_spider import MazeSnkrsSpider
 from crawler.crawler.spiders.maze_novidades_spider import MazeNovidadesSpider
@@ -26,7 +26,7 @@ from crawler.crawler.spiders.artwalk_novidades_spider import ArtwalkNovidadesSpi
 from crawler.crawler.spiders.magicfeet_snkrs_spider import MagicfeetSnkrsSpider
 
 
-from crawler.crawler.spiders.nike_test_spider import NikeTestSpider
+
 
 
 from discord.ext import tasks
@@ -89,6 +89,19 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logado...')
         print('Primeira vez. ',self.created)
+        send_to = self.get_channel(873450794404425779)
+        #await run_spider(NikeTestSpider, send_to)
+
+        embed = discord.Embed(title="Your title here", description="Your desc here", color=3066993) #,color=Hex code        
+        embed.set_thumbnail(url="https://images.lojanike.com.br/500x500/produto/tenis-nike-sb-zoom-stefan-janoski-slip-rm-unissex-CU9230-400-1.jpg")        
+        embed.set_image(url="https://images.lojanike.com.br/500x500/produto/tenis-nike-sb-zoom-stefan-janoski-slip-rm-unissex-CU9230-400-1.jpg")                
+        embed.add_field(name="Tamanhos", value="```diff \n - you can **make** \n as much \nas fields \nyou like to```")        
+        embed.set_footer(text = "footer")
+    #     "footer": {
+    #     "text": "Woah! *So cool!* :smirk:",
+    #     "icon_url": "https://i.imgur.com/fKL31aD.jpg"
+    #   },
+        await send_to.send(embed=embed)
 
     async def sender(self, message):
        
@@ -96,120 +109,112 @@ class MyClient(discord.Client):
 
     @tasks.loop(seconds=600) # task runs every 15 seconds
     async def my_background_task(self):    
-        send_to = self.get_channel(873450794404425779)
-        await run_spider(NikeTestSpider, send_to)
-
-        embed = discord.Embed(title="Your title here", description="Your desc here") #,color=Hex code
-        embed.set_thumbnail(url="https://images.lojanike.com.br/500x500/produto/tenis-nike-sb-zoom-stefan-janoski-slip-rm-unissex-CU9230-400-1.jpg")
-        embed.add_field(name="Nome", value="you can make as much as fields you like to")
-        embed.add_field(name="Valor", value="you can make as much as fields you like to")
-        embed.add_field(name="Opções", value="you can make as much as fields you like to")
-        
+       
                 
         
         #await send_to.send(embed=embed)  
 
-        return 
+         
 
 
 
 
-        spiders = [
-            NikeSnkrsSpider,
-            NikeCalendarioSpider,
-            NikeNovidadesSpider,
-            MazeSnkrsSpider,
-            MazeNovidadesSpider,
-            GdlpSnkrsSpider,
-            GdlpNovidadesSpider,
-            ArtwalkSnkrsSpider,
-            ArtwalkCalendarioSpider,
-            ArtwalkNovidadesSpider,
-            MagicfeetSnkrsSpider,
-        ]    
+        # spiders = [
+        #     NikeSnkrsSpider,
+        #     NikeCalendarioSpider,
+        #     NikeNovidadesSpider,
+        #     MazeSnkrsSpider,
+        #     MazeNovidadesSpider,
+        #     GdlpSnkrsSpider,
+        #     GdlpNovidadesSpider,
+        #     ArtwalkSnkrsSpider,
+        #     ArtwalkCalendarioSpider,
+        #     ArtwalkNovidadesSpider,
+        #     MagicfeetSnkrsSpider,
+        # ]    
         
-        channels = [
-            # {
-            # 'spider': 'nike_snkrs',
-            # 'id': 873217111659532288,
-            # 'categoria':'restock',            
-            # 'mensagem' : 'Novo produto inserido no restock'                
-            # },
-            # {
-            # 'spider': 'nike_novidades',
-            # 'id': 873217865686323260,
-            # 'categoria':'nov-calcados',            
-            # 'mensagem' : 'Novo produto inserido em novidade calçados'                
-            # },
-            # {
-            # 'spider': 'nike_novidades',
-            # 'id': 873217679811559534,
-            # 'categoria':'nov-acessorios',            
-            # 'mensagem' : 'Novo produto inserido em acessórios'                
-            # },
-            # {
-            # 'spider': 'nike_novidades',
-            # 'id': 873217679811559534,
-            # 'categoria':'nov-roupas',            
-            # 'mensagem' : 'Novo produto inserido em roupas'                
-            # },
+        # channels = [
+        #     # {
+        #     # 'spider': 'nike_snkrs',
+        #     # 'id': 873217111659532288,
+        #     # 'categoria':'restock',            
+        #     # 'mensagem' : 'Novo produto inserido no restock'                
+        #     # },
+        #     # {
+        #     # 'spider': 'nike_novidades',
+        #     # 'id': 873217865686323260,
+        #     # 'categoria':'nov-calcados',            
+        #     # 'mensagem' : 'Novo produto inserido em novidade calçados'                
+        #     # },
+        #     # {
+        #     # 'spider': 'nike_novidades',
+        #     # 'id': 873217679811559534,
+        #     # 'categoria':'nov-acessorios',            
+        #     # 'mensagem' : 'Novo produto inserido em acessórios'                
+        #     # },
+        #     # {
+        #     # 'spider': 'nike_novidades',
+        #     # 'id': 873217679811559534,
+        #     # 'categoria':'nov-roupas',            
+        #     # 'mensagem' : 'Novo produto inserido em roupas'                
+        #     # },
 
-            {
-            'spider': 'nike_novidades',
-            'id': 873450794404425779,
-            'categoria':'nov-roupas',            
-            'mensagem' : 'Novo produto generico'                
-            }    
+        #     {
+        #     'spider': 'nike_novidades',
+        #     'id': 873450794404425779,
+        #     'categoria':'nov-roupas',            
+        #     'mensagem' : 'Novo produto generico'                
+        #     }    
 
 
                     
-        ]
+        # ]
 
-        spiders_db = [
-            'artwalk_calendario',
-            'artwalk_novidades',
-            'artwalk_snkrs',
-            'gdlp_novidades',
-            'gdlp_snkrs',
-            'magicfeet_snkrs',
-            'maze_novidades',
-            'maze_snkrs',
-            'nike_calendario',
-            'nike_novidades',
-            'nike_snkrs',
-        ]
-        send_to = self.get_channel(873450794404425779)
-        await send_to.send('Ferificacao inicializada')        
-        for spider in spiders:            
-            await run_spider(spider)            
+        # spiders_db = [
+        #     'artwalk_calendario',
+        #     'artwalk_novidades',
+        #     'artwalk_snkrs',
+        #     'gdlp_novidades',
+        #     'gdlp_snkrs',
+        #     'magicfeet_snkrs',
+        #     'maze_novidades',
+        #     'maze_snkrs',
+        #     'nike_calendario',
+        #     'nike_novidades',
+        #     'nike_snkrs',
+        # ]
+        # send_to = self.get_channel(873450794404425779)
+        # await send_to.send('Ferificacao inicializada')        
+        # for spider in spiders:            
+        #     await run_spider(spider)            
 
-            if self.created == False:
-                cursor.execute("update products set send='avisado'")
-                database.commit()  
+        #     if self.created == False:
+        #         cursor.execute("update products set send='avisado'")
+        #         database.commit()  
 
-            if self.created:           
-                for spider in spiders_db:                    
-                    query = 'SELECT name, url, id FROM products where send="avisar" and spider="{}"'.format(spider)
+        #     if self.created:           
+        #         for spider in spiders_db:                    
+        #             query = 'SELECT name, url, id FROM products where send="avisar" and spider="{}"'.format(spider)
 
-                    rows = [[str(row[0]).strip(),str(row[1]).strip(), str(row[2]).strip()]  for row in cursor.execute(query)]            
-                    for row in rows:
-                        print('Enviando mensagem para channel generico')
-                        await send_to.send("{}:\n {}\n{}".format('Novo produto generico' , row[0], row[1]))                        
-                        cursor.execute("update products set send='avisado' where id='"+row[2]+"'")
-                        database.commit()               
+        #             rows = [[str(row[0]).strip(),str(row[1]).strip(), str(row[2]).strip()]  for row in cursor.execute(query)]            
+        #             for row in rows:
+        #                 print('Enviando mensagem para channel generico')
+        #                 await send_to.send("{}:\n {}\n{}".format('Novo produto generico' , row[0], row[1]))                        
+        #                 cursor.execute("update products set send='avisado' where id='"+row[2]+"'")
+        #                 database.commit()               
 
-            # if self.created:           
-            #     for channel in channels:
-            #         send_to = self.get_channel(channel['id'])
-            #         query = 'SELECT name, url, id FROM products where send="avisar" and spider="{}" and categoria="{}"'.format(channel['spider'],channel['categoria'])
+        #     # if self.created:           
+        #     #     for channel in channels:
+        #     #         send_to = self.get_channel(channel['id'])
+        #     #         query = 'SELECT name, url, id FROM products where send="avisar" and spider="{}" and categoria="{}"'.format(channel['spider'],channel['categoria'])
 
-            #         rows = [[str(row[0]).strip(),str(row[1]).strip(), str(row[2]).strip()]  for row in cursor.execute(query)]            
-            #         for row in rows:
-            #             print('Enviando mensagem para channel {}'.format(channel['id']))
-            #             await send_to.send("{}:\n {}\n{}".format(channel['mensagem'], row[0], row[1]))                        
-            #             cursor.execute("update products set send='avisado' where id='"+row[2]+"'")
-            #             database.commit()
-        await send_to.send('Ferificacao finalizada') 
+        #     #         rows = [[str(row[0]).strip(),str(row[1]).strip(), str(row[2]).strip()]  for row in cursor.execute(query)]            
+        #     #         for row in rows:
+        #     #             print('Enviando mensagem para channel {}'.format(channel['id']))
+        #     #             await send_to.send("{}:\n {}\n{}".format(channel['mensagem'], row[0], row[1]))                        
+        #     #             cursor.execute("update products set send='avisado' where id='"+row[2]+"'")
+        #     #             database.commit()
+        # await send_to.send('Ferificacao finalizada') 
         
         self.created = True
         
